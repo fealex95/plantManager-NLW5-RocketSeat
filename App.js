1675dbad43c5788c24cf217522267c7e9517464a
Felipe Alexandre <felipealexandre98@gmail.com>
2021-04-26 18:04:11 -0300

    Aula 02 - NLW5 - React Native

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Confirmation } from './src/pages/Confirmation'
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost'
+
+import Routes from './src/routes'
+
+import AppLoading from 'expo-app-loading'
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+export function Button({ title, onPress }) {
+    return (
+        <TouchableOpacity style={styles.container} onPress={onPress}>
+            <Text style={styles.text}>
+                {title}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.green,
+        height: 56,
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    text: {
+        fontSize: 16,
+        color: colors.white,
+        fontFamily: fonts.heading
+    }
+})
